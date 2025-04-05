459dcdacca7559b333944675d4893d33741c61cd
kino <kinogtag@outlook.com>
2025-04-05 21:07:18 +0100

    GUI Overhaul
    Added the ability to drag the GUI,
    Added pages to the GUI
    Organized the GUI

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,6 @@
-﻿using BepInEx;
+using BepInEx;
 using GorillaNetworking;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace GamemodeSelectorGUI
 {
@@ -14,43 +13,60 @@ namespace GamemodeSelectorGUI
             HarmonyPatches.ApplyHarmonyPatches();
         }
 
+        int currentPage = 0;
+        bool guiEnabled = true;
+        Rect windowRect = new Rect(50, 50, 250, 240); // x, y, width, height
 
-        public void Update()
-        {
-            if (Keyboard.current.tabKey.wasPressedThisFrame)
-            {
-                guiEnabled = !guiEnabled;
-            }
-        }
-
-        public bool guiEnabled = true;
         public void OnGUI()
         {
             if (guiEnabled)
             {
-                GUI.Box(new Rect(50, 50, 175, 180), Constants.Name);
-                GUI.color = Color.gray;
-                GUI.Label(new Rect(55, 65, 150, 20), Constants.Version);
-                GUI.Label(new Rect(170, 65, 150, 20), "ciperuoy");
+                windowRect = GUI.Window(0, windowRect, DrawWindow, Constants.Name);
+            }
+        }
 
-                if (GUI.Button(new Rect(55, 90, 165, 30), "Casual"))
-                {
+        void DrawWindow(int windowID)
+        {
+            GUI.color = Color.gray;
+            GUI.Label(new Rect(10, 15, 150, 20), Constants.Version);
+            GUI.Label(new Rect(165, 15, 150, 20), "ciperuoy");
+
+            if (currentPage == 0)
+            {
+                if (GUI.Button(new Rect(10, 40, 230, 30), "Casual"))
                     GorillaComputer.instance.currentGameMode.Value = "Casual";
-                }
-                if (GUI.Button(new Rect(55, 125, 165, 30), "Infection"))
-                {
+
+                if (GUI.Button(new Rect(10, 75, 230, 30), "Infection"))
                     GorillaComputer.instance.currentGameMode.Value = "Infection";
-                }
-                if (GUI.Button(new Rect(55, 160, 165, 30), "Modded Casual"))
-                {
+
+                if (GUI.Button(new Rect(10, 110, 230, 30), "Guardian"))
+                    GorillaComputer.instance.currentGameMode.Value = "Guardian";
+
+                if (GUI.Button(new Rect(10, 145, 230, 30), "Freezetag"))
+                    GorillaComputer.instance.currentGameMode.Value = "Freezetag";
+            }
+            else if (currentPage == 1)
+            {
+                if (GUI.Button(new Rect(10, 40, 230, 30), "Modded Casual"))
                     GorillaComputer.instance.currentGameMode.Value = "MODDED_Casual";
-                }
-                if (GUI.Button(new Rect(55, 195, 165, 30), "Modded Infection"))
-                {
+
+                if (GUI.Button(new Rect(10, 75, 230, 30), "Modded Infection"))
                     GorillaComputer.instance.currentGameMode.Value = "MODDED_Infection";
-                }
+
+                if (GUI.Button(new Rect(10, 110, 230, 30), "Modded Guardian"))
+                    GorillaComputer.instance.currentGameMode.Value = "MODDED_Guardian";
+
+                if (GUI.Button(new Rect(10, 145, 230, 30), "Modded Freezetag"))
+                    GorillaComputer.instance.currentGameMode.Value = "MODDED_Freezetag";
             }
 
+            if (GUI.Button(new Rect(210, 185, 30, 20), "→"))
+                currentPage = (currentPage + 1) % 2;
+
+            if (GUI.Button(new Rect(10, 185, 30, 20), "←"))
+                currentPage = (currentPage - 1 + 2) % 2;
+
+            GUI.DragWindow(new Rect(0, 0, 10000, 20));
         }
     }
 }
